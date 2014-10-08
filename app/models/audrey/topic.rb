25918972f8bfdd54937d007368f4b1d536b805e5
Nick Nadboralski <nnadboralski@unite4good.org>
2014-10-08 10:40:03 -0500

    Adding voting score to Topic

--- app/models/audrey/topic.rb
+++ app/models/audrey/topic.rb
@@ -2,5 +2,14 @@ module Audrey
   class Topic < ActiveRecord::Base
     belongs_to :owner, class_name: Audrey.owner_class.to_s
     has_many :votes
+
+    validates_presence_of :topic
+    validates_presence_of :content
+    validates_presence_of :owner
+
+    def score
+      Audrey::Vote.where(id: self.id).sum(:value)
+    end
+
   end
 end
