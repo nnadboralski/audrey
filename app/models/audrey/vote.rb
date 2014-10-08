bd207d1c54473f00b64b4798db7fbb56d662fa63
Nick Nadboralski <nnadboralski@unite4good.org>
2014-10-08 08:41:49 -0500

    Fleshing out vote model

--- app/models/audrey/vote.rb
+++ app/models/audrey/vote.rb
@@ -1,4 +1,12 @@
 module Audrey
   class Vote < ActiveRecord::Base
+    belongs_to :user, class_name: Audrey.owner_class.to_s
+    belongs_to :topic
+
+    validates_uniqueness_of :user_id, scope: :topic_id
+
+    validates_presence_of :user
+    validates_presence_of :topic
+    validates_presence_of :value
   end
 end
--- db/migrate/20141007205411_create_audrey_votes.rb
+++ db/migrate/20141007205411_create_audrey_votes.rb
@@ -2,6 +2,10 @@ class CreateAudreyVotes < ActiveRecord::Migration
   def change
     create_table :audrey_votes do |t|
 
+      t.references :user, index: true
+      t.references :topic, index: true
+      t.integer :value
+
       t.timestamps
     end
   end
